88b4588480420c3921d332f83d63c878176de87f
GongZheng <cis.gong@gmail.com>
2020-12-28 11:15:11 +0800

    Add files via upload

---
+++ Optimized_C/LUT_Fishingrod.c
@@ -0,0 +1,8 @@
+/*
+ *  Fishingrod.h
+ *
+ *  Description: Fishingrod-128 with a look-up table (LUT) structure.
+ *  Created on: 2020-12-28
+ *  Last modified: 2020-12-28
+ *  Coding: Zheng Gong
+ */
---
+++ Standard_C/Fishingrod.c
@@ -0,0 +1,8 @@
+/*
+ *  Fishingrod.h
+ *
+ *  Description: Fishingrod-128 with a clear structure.
+ *  Created on: 2020-12-28
+ *  Last modified: 2020-12-28
+ *  Coding: Zheng Gong
+ */
