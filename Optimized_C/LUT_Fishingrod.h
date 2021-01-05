9aa5014b49747f338a3c67e5316cd052994d7cf0
gongz <cis.gong@gmail.com>
2021-01-05 19:48:36 +0800

    Remove the last modify info from codes (editorial)

--- Optimized_C/LUT_Fishingrod.h
+++ Optimized_C/LUT_Fishingrod.h
@@ -3,8 +3,7 @@
  *
  *  Description: Fishingrod-128 with a look-up table (LUT) structure.
  *  Created on: 2020-12-28
- *  Last modified: 2020-12-28
- *  Coding: Zheng Gong
+ *  Coding: Zheng Gong, Jinhai Chen
  */
 
 #ifndef LUT_FISHINGROD_H_
--- Standard_C/Fishingrod.h
+++ Standard_C/Fishingrod.h
@@ -3,8 +3,7 @@
  *
  *  Description: Fishingrod-128 with a clear structure.
  *  Created on: 2020-12-28
- *  Last modified: 2020-12-28
- *  Coding: Zheng Gong
+ *  Coding: Zheng Gong, Jinhai Chen.
  */
 
 #ifndef FISHINGROD_H_
